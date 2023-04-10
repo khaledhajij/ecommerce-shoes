d3ac0c0aba70324f7151643076805e8ab92c99c1
khaledhajij <hajij94@gmail.com>
2023-04-10 23:53:51 +0300

    Making routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(()=>{
+    axios.get('/api/shoes').then(res => console.log(res))
+    // axios.get('/api/categories').then(res => console.log(res))
+    axios.get('/api/lists').then(res => console.log(res))
+    axios.get('/api/lists/1/shoes').then(res => console.log(res))
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
